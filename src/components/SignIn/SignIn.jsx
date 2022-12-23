f5aaaed4566ce8f11412d14cb9b1189715519a26
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-23 14:49:46 +0300

    feat: create signin page

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -4,17 +4,22 @@ import styles from './SignIn.module.css';
 function SignIn() {
     return (
         <div className={styles.container}>
-            <div>
-                <h1 className={styles.title}>Sign in</h1>
-                <div>
-                    <h2>Email</h2>
-                    <input type="email"/>
-                </div>
-                <div>
-                    <h2>Email</h2>
-                    <input type="email"/>
-                </div>
+            <div className={styles.containerForm}>
+                <form className={styles.form}>
+                    <h1 className={styles.title}>Sign in</h1>
+                    <div className={styles.containerEmail}>
+                        <h2 className={styles.fieldName}>Email</h2>
+                        <input type="email" className={styles.field} placeholder="Email" />
+                    </div>
+                    <div className={styles.containerPassword}>
+                        <h2 className={styles.fieldName}>Password</h2>
+                        <input type="email" className={styles.field} placeholder="Enter password" />
+                    </div>
+                    <button type="submit" className={styles.submitButton}>Log in</button>
+                    <a href="/#" className={styles.forgotPassword}>Forgot password?</a>
+                </form>
             </div>
+            <div className={styles.backgroundImg}></div>
         </div>
     );
 }
