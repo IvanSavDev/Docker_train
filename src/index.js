08738c2b135385a8f4c2e8b8d16ff6aa241f48d4
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-23 14:52:22 +0300

    feat: add app and signin routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
 import App from './components/App';
+import SignIn from "./components/SignIn/SignIn";
+
+import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/signin",
+        element: <SignIn />
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
