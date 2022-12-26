8cf713beddca49331f631d9940a40a93b3ec87e8
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-26 18:12:40 +0300

    feat: implement signUp page

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,38 +1,124 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
 
+import {
+    ACCOUNT_EXISTS_ERROR, COMPANY_NAME_ERROR,
+    EMAIL_ERROR,
+    FULLNAME_ERROR,
+    MATCH_PASSWORD_ERROR,
+    PASSWORD_ERROR, Paths
+} from "../../consts/consts";
 import ContainerSignInAndSignUp from '../ContainerSignInAndSignUp';
-import InputWithTitle from "../Inputs/InputWithTitle";
+import Input from "../Inputs/Input";
 import Button from "../Buttons/Button";
 import Form from "../Form/Form";
 import RouteLink from "../RouteLink/RouteLink";
+import {
+    isMatchPassword,
+    isValidCompanyName,
+    isValidEmail,
+    isValidFullName,
+    isValidPassword
+} from "../../utils/validation";
+import {getDataFromLocalStorage, setDataInLocalStorage} from "../../utils/localStorage";
+import {checkExistsAccountByEmail, isEmptyProperties} from "../../utils/utils";
+import {useForm} from "../Hooks/useForm";
 
 import styles from './SignUp.module.css';
 
 function SignIn() {
+    const [errors, setErrors] = useState({
+        name: null,
+        surname: null,
+        companyName: null,
+        email: null,
+        password: null,
+        repeatPassword: null,
+        accountExists: null,
+    });
+    const [form, setForm] = useForm({
+        name: '',
+        surname: '',
+        companyName: '',
+        email: '',
+        password: '',
+        repeatPassword: ''
+    })
+    const [accounts, setAccounts] = useState([]);
+    const [checkAccountExists, setCheckAccountExists] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setAccounts(getDataFromLocalStorage('accounts'));
+    }, [errors]);
+
+    useEffect(() => {
+        const isNotErrors = Object.values(errors).every(error => !error);
+        if (isNotErrors && checkAccountExists) {
+            setDataInLocalStorage('accounts', [...accounts, form]);
+            navigate(Paths.main);
+        }
+    });
+
+    const handleChange = (event) => {
+        setCheckAccountExists(false);
+        setForm(event);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { name, surname, companyName, email, password, repeatPassword } = form;
+
+        const isExistsAccount = checkExistsAccountByEmail(accounts, email);
+
+        const checkedErrors = {
+            name: isValidFullName(name) ? null : FULLNAME_ERROR,
+            surname: isValidFullName(surname) ? null : FULLNAME_ERROR,
+            companyName: isValidCompanyName(companyName) ? null : COMPANY_NAME_ERROR,
+            email: isValidEmail(email) ? null : EMAIL_ERROR,
+            password: isValidPassword(password) ? null : PASSWORD_ERROR,
+            repeatPassword: isMatchPassword(password, repeatPassword) ? null : MATCH_PASSWORD_ERROR,
+            accountExists: !isExistsAccount ? null : ACCOUNT_EXISTS_ERROR,
+        };
+
+        setErrors(checkedErrors);
+        setCheckAccountExists(true);
+    };
+
     return (
         <ContainerSignInAndSignUp>
-            <Form formName="Create an account">
-                <div className={styles.fullName}>
-                    <InputWithTitle type="text" placeholder="First name" titleName="First name" />
-                    <InputWithTitle type="text" placeholder="Last name" titleName="Last name" />
+            <Form formName="Create an account" onSubmit={handleSubmit}>
+                <div className={styles.containerFullName}>
+                    <Input type="text" name="name" placeholder="First name" titleName="First name" error={errors.name} onChange={handleChange} />
+                    <Input type="text" name="surname" placeholder="Last name" titleName="Last name" error={errors.surname} onChange={handleChange} />
                 </div>
                 <div className={styles.containerInput}>
-                    <InputWithTitle type="text" placeholder="Company name" titleName="Company name" />
+                    <Input type="text" name="companyName" placeholder="Company name" titleName="Company name" error={errors.companyName} onChange={handleChange} />
                 </div>
                 <div className={styles.containerInput}>
-                    <InputWithTitle type="email" placeholder="Email" titleName="Email" />
+                    <Input type="text" name="email" placeholder="Email" titleName="Email" error={errors.email} onChange={handleChange}/>
                 </div>
                 <div className={styles.containerInput}>
-                    <InputWithTitle type="password" placeholder="Enter password" titleName="Password" />
+                    <Input type="password" name="password" placeholder="Enter password" titleName="Password" error={errors.password} onChange={handleChange} />
                 </div>
                 <div className={styles.containerPassword}>
-                    <InputWithTitle type="password" placeholder="Repeat password" titleName="Repeat password" />
+                    <Input type="password" name="repeatPassword" placeholder="Repeat password" titleName="Repeat password" error={errors.repeatPassword} onChange={handleChange} />
                 </div>
-                <Button type="submit">Create account</Button>
+                <div className={styles.containerError}>
+                    {errors.accountExists
+                        && checkAccountExists
+                        && !isEmptyProperties(form)
+                        ? <p className={styles.accountExists}>Account already exist</p>
+                        : ''
+                    }
+                </div>
+                <div className={styles.containerButton}>
+                    <Button type="submit">Create account</Button>
+                </div>
             </Form>
             <div>
-                <span>Already have an account? </span>
-                <RouteLink href="/#">Log in</RouteLink>
+                <span className={styles.alreadyHaveAccount}>Already have an account? </span>
+                <RouteLink to={Paths.signIn}>Log in</RouteLink>
             </div>
         </ContainerSignInAndSignUp>
     );
