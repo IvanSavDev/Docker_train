950db708e14048533656aea3bdaca0fab9f8cdaf
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-26 17:47:46 +0300

    feat: add function for set values to localStorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,1 +1,3 @@
-export const getAccounts = (key) => JSON.parse(localStorage.getItem(key)) || [];
+export const getDataFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) || [];
+
+export const setDataInLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
