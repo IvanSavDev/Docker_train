31b88fd5c7c5c6ee4a26ad13e83bd8ed220ba065
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-26 16:15:44 +0300

    feat: add validation functions

---
+++ src/utils/validation.js
@@ -0,0 +1,9 @@
+import { REG_EMAIL, REG_PASSWORD } from "../consts/consts";
+
+export const isValidFullName = (name) => name.length >= 3;
+
+export const isValidCompanyName = (name) => name.length >= 1;
+export const isValidEmail = (email) => REG_EMAIL.test(email);
+export const isValidPassword = (password) => REG_PASSWORD.test(password);
+
+export const isMatchPassword = (password, repeatedPassword) => password === repeatedPassword;
