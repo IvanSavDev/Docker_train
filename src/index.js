fba432e0d5cfa6094475ae31849ca9254c15bf55
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-27 10:35:02 +0300

    refactor: delete routers from index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-
 import App from './components/App';
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import {Paths} from "./consts/consts";
 
 import './index.css';
-
-const router = createBrowserRouter([
-    {
-        path: Paths.main,
-        element: <App />,
-    },
-    {
-        path: Paths.signIn,
-        element: <SignIn />
-    },
-    {
-        path: Paths.signUp,
-        element: <SignUp />
-    }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
