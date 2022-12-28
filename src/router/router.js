6917a9308db670777d1c2d95bd867304ac41e3e7
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-28 13:09:49 +0300

    feat: add children routers

--- src/router/router.js
+++ src/router/router.js
@@ -2,53 +2,62 @@ import React from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 
 import {Paths} from "../consts/consts";
-import SignIn from "../Pages/SignIn/SignIn";
-import SignUp from "../Pages/SignUp/SignUp";
-import Main from "../Pages/Main/Main";
-import MyProduct from "../Pages/MyProduct/MyProduct";
-import MySales from "../Pages/MySales/MySales";
-import PersonalCabinet from "../Pages/PersonalCabinet/PersonalCabinet";
+import SignIn from "../pages/SignIn/SignIn";
+import SignUp from "../pages/SignUp/SignUp";
+import Main from "../pages/Main/Main";
+import MyProduct from "../pages/MyProduct/MyProduct";
+import MySales from "../pages/MySales/MySales";
+import PersonalCabinet from "../pages/PersonalCabinet/PersonalCabinet";
 import useAuth from "../hooks/useAuth";
+import PersonalPage from "../pages/PersonalPage";
 
 const routers = [
     {
         path: Paths.signUp,
         element: <SignUp/>,
-        private: false
+        private: false,
     },
     {
         path: Paths.signIn,
         element: <SignIn/>,
-        private: false
+        private: false,
     },
     {
         path: '*',
         element: <Navigate to={Paths.signIn}/>,
-        private: false
+        private: false,
     },
     {
         path: Paths.base,
-        element: <Main/>,
-        private: true
-    },
-    {
-        path: Paths.myProduct,
-        element: <MyProduct/>,
-        private: true
-    },
-    {
-        path: Paths.mySales,
-        element: <MySales/>,
-        private: true
-    },
-    {
-        path: Paths.personalCabinet,
-        element: <PersonalCabinet/>,
-        private: true
+        element: <PersonalPage/>,
+        private: true,
+        children: [
+            {
+                path: Paths.main,
+                element: <Main/>,
+                private: true,
+                index: true
+            },
+            {
+                path: Paths.myProduct,
+                element: <MyProduct/>,
+                private: true
+            },
+            {
+                path: Paths.mySales,
+                element: <MySales/>,
+                private: true
+            },
+            {
+                path: Paths.personalCabinet,
+                element: <PersonalCabinet/>,
+                private: true
+            },
+        ]
     },
     {
         path: '*',
-        element: <Navigate to={Paths.main}/>,
+        element: <Navigate to={Paths.base}/>,
         private: true
     }
 
@@ -56,11 +65,27 @@ const routers = [
 
 const AppRouter = () => {
     const {loggedIn} = useAuth();
-    console.log(loggedIn)
+
     return <Routes>
         {routers
             .filter(router => loggedIn ? router.private : !router.private)
-            .map((router, index) => <Route key={index} path={router.path} element={router.element}/>)
+            .map((router, index) => (
+                <Route key={index} path={router.path} element={router.element}>
+                    {router?.children?.map((childRouter, index) => (
+                        childRouter.index
+                            ? <Route
+                                key={index}
+                                element={childRouter.element}
+                                index
+                            />
+                            : <Route
+                                key={index}
+                                path={childRouter.path}
+                                element={childRouter.element}
+                            />
+                    ))}
+                </Route>
+            ))
         }
     </Routes>
 }
