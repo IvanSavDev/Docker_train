af3c5788a3d328533b773998ee2a9cbe58e9fba9
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-28 13:04:02 +0300

    feat: add checkNewEmailOnValidation and isDifferenceWithOldAccount functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,22 @@
 export const isEmptyProperties = (object) => {
     return Object.keys(object).some((key) => object[key].length === 0);
-}
+};
+
 export const checkExistsAccountByEmail = (accounts, email) => {
     const account = accounts.find((account) => account.email === email);
     return Boolean(account);
 };
+
+export const checkNewEmailOnValidation = (accounts, oldEmail, newEmail) => {
+    if (oldEmail === newEmail) {
+        return false;
+    }
+    return checkExistsAccountByEmail(accounts, newEmail);
+};
+
+export const isDifferencesWithOldAccount = (oldAccount, newAccount) => {
+    return Object.keys(oldAccount).every(key => oldAccount[key] === newAccount[key]);
+}
 
 export const checkExistsAccountByEmailAndPassword = (accounts, email, password) => {
     const account = accounts.find((account) => account.email === email && account.password === password);
