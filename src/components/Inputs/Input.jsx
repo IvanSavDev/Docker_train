c86d7e3d62792bf2200a6afb8dba7bef60a9c12c
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-30 17:32:31 +0300

    feat: add condition for title input

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -5,7 +5,7 @@ import styles from './Input.module.css';
 const Input = ({ titleName, error, containerStyles, ...rest }) => {
     return (
         <div className={containerStyles}>
-            <h2 className={styles.fieldName}>{titleName}</h2>
+            {titleName && <h2 className={styles.fieldName}>{titleName}</h2>}
             <input className={styles.field} {...rest} />
             <p className={styles.textError}>{error}</p>
         </div>
