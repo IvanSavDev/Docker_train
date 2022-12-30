31b61ee108112a9865a95b15f3d0e871f95abd98
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-30 10:31:11 +0300

    refactor: add indents

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
 import Header from "../../components/Header/Header";
 
-
-import styles from './Main.module.css';
+import styles from "./Main.module.css";
 import PieChart from "../../components/Diagrams/PieChart";
 
-
-
 const Main = () => {
-    return (
-        <div>
-            <Header title="Sales statistics" description="Welcome to CRM dashboard"/>
-            <div className={styles.container}>
-                <PieChart />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header title="Sales statistics" description="Welcome to CRM dashboard" />
+      <div className={styles.container}>
+        <PieChart />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,3 +1,5 @@
-export const getDataFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) || [];
+export const getDataFromLocalStorage = (key) =>
+  JSON.parse(localStorage.getItem(key));
 
-export const setDataInLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+export const setDataInLocalStorage = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data));
