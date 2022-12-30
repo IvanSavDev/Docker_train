d96ae39657728388d65f8f6464586fea47032230
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-30 17:33:02 +0300

    feat: add providers in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import AppRouter from "../router/router";
 import AuthProvider from "../hoc/AuthProvider";
+import AccountProvider from "../hoc/AccountProvider";
+import ProductsProvider from "../hoc/ProductsProvider";
+import SalesProvider from "../hoc/SalesProvider";
 
 const App = () => (
-    <AuthProvider>
-        <BrowserRouter>
-            <AppRouter/>
-        </BrowserRouter>
-    </AuthProvider>
+  <AuthProvider>
+    <AccountProvider>
+      <ProductsProvider>
+        <SalesProvider>
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </SalesProvider>
+      </ProductsProvider>
+    </AccountProvider>
+  </AuthProvider>
 );
 
 export default App;
