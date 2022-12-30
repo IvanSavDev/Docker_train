3f399b1c17d4b01e8f626031182af0132ca30c15
Ivan Savenko <iv.savenko@it-justice.com>
2022-12-30 17:24:51 +0300

    refactor: add indent for router file

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import {Paths} from "../consts/consts";
+import { Paths } from "../consts/consts";
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
 import Main from "../pages/Main/Main";
@@ -10,84 +10,83 @@ import MySales from "../pages/MySales/MySales";
 import PersonalCabinet from "../pages/PersonalCabinet/PersonalCabinet";
 import useAuth from "../hooks/useAuth";
 import PersonalPage from "../pages/PersonalPage";
+import useAccount from "../hooks/useAccount";
 
 const routers = [
-    {
-        path: Paths.signUp,
-        element: <SignUp/>,
-        private: false,
-    },
-    {
-        path: Paths.signIn,
-        element: <SignIn/>,
-        private: false,
-    },
-    {
-        path: '*',
-        element: <Navigate to={Paths.signIn}/>,
-        private: false,
-    },
-    {
-        path: Paths.base,
-        element: <PersonalPage/>,
+  {
+    path: Paths.signUp,
+    element: <SignUp />,
+    private: false,
+  },
+  {
+    path: Paths.signIn,
+    element: <SignIn />,
+    private: false,
+  },
+  {
+    path: "*",
+    element: <Navigate to={Paths.signIn} />,
+    private: false,
+  },
+  {
+    path: Paths.base,
+    element: <PersonalPage />,
+    private: true,
+    children: [
+      {
+        path: Paths.main,
+        element: <Main />,
         private: true,
-        children: [
-            {
-                path: Paths.main,
-                element: <Main/>,
-                private: true,
-                index: true
-            },
-            {
-                path: Paths.myProduct,
-                element: <MyProduct/>,
-                private: true
-            },
-            {
-                path: Paths.mySales,
-                element: <MySales/>,
-                private: true
-            },
-            {
-                path: Paths.personalCabinet,
-                element: <PersonalCabinet/>,
-                private: true
-            },
-        ]
-    },
-    {
-        path: '*',
-        element: <Navigate to={Paths.base}/>,
-        private: true
-    }
-
-]
+        index: true,
+      },
+      {
+        path: Paths.myProduct,
+        element: <MyProduct />,
+        private: true,
+      },
+      {
+        path: Paths.mySales,
+        element: <MySales />,
+        private: true,
+      },
+      {
+        path: Paths.personalCabinet,
+        element: <PersonalCabinet />,
+        private: true,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to={Paths.base} />,
+    private: true,
+  },
+];
 
 const AppRouter = () => {
-    const {loggedIn} = useAuth();
+  const { loggedIn } = useAuth();
 
-    return <Routes>
-        {routers
-            .filter(router => loggedIn ? router.private : !router.private)
-            .map((router, index) => (
-                <Route key={index} path={router.path} element={router.element}>
-                    {router?.children?.map((childRouter, index) => (
-                        childRouter.index
-                            ? <Route
-                                key={index}
-                                element={childRouter.element}
-                                index
-                            />
-                            : <Route
-                                key={index}
-                                path={childRouter.path}
-                                element={childRouter.element}
-                            />
-                    ))}
-                </Route>
-            ))
-        }
+  return (
+    <Routes>
+      {routers
+        .filter((router) => (loggedIn ? router.private : !router.private))
+        .map((router, index) => (
+          <Route key={index} path={router.path} element={router.element}>
+            {router?.children?.map((childRouter, index) =>
+              childRouter.index ? (
+                <Route key={index} element={childRouter.element} index />
+              ) : (
+                <Route
+                  key={index}
+                  path={childRouter.path}
+                  element={childRouter.element}
+                />
+              )
+            )}
+          </Route>
+        ))}
     </Routes>
-}
+  );
+};
 
 export default AppRouter;
