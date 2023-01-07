7eba0a60982089e1158047315ef469b65504d787
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-07 22:25:34 +0300

    feat: add theme for materialUI components

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,54 @@ import AuthProvider from "../hoc/AuthProvider";
 import AccountProvider from "../hoc/AccountProvider";
 import ProductsProvider from "../hoc/ProductsProvider";
 import SalesProvider from "../hoc/SalesProvider";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "InterRegular, InterMedium, InterBold",
+  },
+  palette: {
+    custom: {
+      main: {
+        grey: "#2B3844",
+      },
+    },
+  },
+
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          fontSize: "14px",
+          lineHeight: "1em",
+          height: "auto",
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          fontSize: "14px",
+        },
+      },
+    },
+  },
+});
 
 const App = () => (
-  <AuthProvider>
-    <AccountProvider>
-      <ProductsProvider>
-        <SalesProvider>
-          <BrowserRouter>
-            <AppRouter />
-          </BrowserRouter>
-        </SalesProvider>
-      </ProductsProvider>
-    </AccountProvider>
-  </AuthProvider>
+  <ThemeProvider theme={theme}>
+    <AuthProvider>
+      <AccountProvider>
+        <ProductsProvider>
+          <SalesProvider>
+            <BrowserRouter>
+              <AppRouter />
+            </BrowserRouter>
+          </SalesProvider>
+        </ProductsProvider>
+      </AccountProvider>
+    </AuthProvider>
+  </ThemeProvider>
 );
 
 export default App;
