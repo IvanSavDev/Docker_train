e2c389f7b9900946ab1adfb6a0bfc624a18fb487
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-09 18:40:10 +0300

    feat: realize sales page

--- src/components/Modals/EditProduct.jsx
+++ src/components/Modals/EditProduct.jsx
@@ -1,99 +1,139 @@
 import React, { useLayoutEffect, useState } from "react";
-
 import DialogActions from "@mui/material/DialogActions";
-import { useForm } from "../../hooks/useForm";
-import { Errors } from "../../consts/consts";
-import { capitalizeFirstLetter, haveErrors } from "../../utils/utils";
+
+import useForm from "../../hooks/useForm";
+import useProducts from "../../hooks/useProducts";
+import useAccount from "../../hooks/useAccount";
 import StandardButton from "../Buttons/StandardButton";
-import ModalInput from "./ModalInput";
+import Input from "../Inputs/Input";
 import ModalTitle from "./ModalTitle";
 import ModalInputContainer from "./ModalInputContainer";
 import ModalContainer from "./ModalContainer";
-import useProducts from "../../hooks/useProducts";
+import { Errors } from "../../consts/consts";
+import { getKeysDifferentFields, haveErrors } from "../../utils/utils";
 
-const initialStateForm = {
-  store: "",
-  price: "",
-  productName: "",
-  category: "",
-  remains: "",
-  weight: "",
-};
-
-const initialStateErrors = {
-  store: null,
-  price: null,
-  productName: null,
-  category: null,
-  remains: null,
-  weight: null,
-};
-
-const fieldsNames = [
-  "store",
-  "price",
-  "productName",
-  "category",
-  "remains",
-  "weight",
-];
-
-const EditProduct = ({ open, closeModal, id }) => {
-  const [errors, setErrors] = useState({ ...initialStateErrors });
-  const [form, setForm] = useForm({ ...initialStateForm });
-  const { getProduct, updateProduct } = useProducts();
+const EditProduct = ({ open, closeModal, productId }) => {
+  const [errors, setErrors] = useState({
+    store: null,
+    price: null,
+    productName: null,
+    category: null,
+    remains: null,
+    weight: null,
+  });
+  const [form, setForm] = useForm({
+    store: "",
+    price: "",
+    productName: "",
+    category: "",
+    remains: "",
+    weight: "",
+  });
+  const { getProduct, updateProduct, addProduct, deleteProduct } =
+    useProducts();
+  const { account } = useAccount();
 
   useLayoutEffect(() => {
-    setForm(getProduct(id));
-  }, []);
+    setForm(getProduct(productId));
+  }, [getProduct, setForm, productId]);
 
   const handleSubmit = () => {
     const { store, price, productName, category, remains, weight } = form;
+
+    const priceAsNumber = Number(price);
+    const remainsAsNumber = Number(remains);
+    const weightAsNumber = Number(weight);
+
     const checkedErrors = {
       store: store.length > 0 ? null : Errors.requiredField,
-      price: Number(price) > 0 ? null : Errors.moreZero,
+      price: priceAsNumber > 0 ? null : Errors.moreZero,
       productName: productName.length > 0 ? null : Errors.requiredField,
       category: category.length > 0 ? null : Errors.requiredField,
-      remains: Number(remains) > 0 ? null : Errors.moreZero,
-      weight: Number(weight) > 0 ? null : Errors.moreZero,
+      remains: remainsAsNumber > 0 ? null : Errors.moreZero,
+      weight: weightAsNumber > 0 ? null : Errors.moreZero,
     };
 
     const isNotErrors = haveErrors(checkedErrors);
-    if (isNotErrors) {
-      updateProduct(id, { ...form });
-      setForm({ ...initialStateForm });
-      closeModal();
-    }
 
-    setErrors(checkedErrors);
+    if (isNotErrors) {
+      const oldProduct = getProduct(productId);
+      const changedFields = getKeysDifferentFields(form, oldProduct);
+      if (changedFields.length === 1 && changedFields[0] === "remains") {
+        updateProduct(productId, { remains: remainsAsNumber });
+      } else {
+        deleteProduct(oldProduct.id);
+        addProduct({
+          ...form,
+          price: priceAsNumber,
+          remains: remainsAsNumber,
+          weight: weightAsNumber,
+          address: account.address,
+        });
+      }
+      closeModal();
+    } else {
+      setErrors(checkedErrors);
+    }
   };
 
-  const handleChange = ({ target }) => setForm({ [target.name]: target.value });
-
-  const handleClose = () => {
-    setForm({ ...initialStateForm });
-    setErrors({ ...initialStateErrors });
-    closeModal();
+  const handleChange = ({ target }) => {
+    setErrors((prevState) => ({ ...prevState, [target.name]: null }));
+    setForm({ [target.name]: target.value });
   };
 
   return (
-    <ModalContainer open={open} onClose={handleClose}>
-      <ModalTitle handleClose={handleClose}>Editing a product</ModalTitle>
+    <ModalContainer open={open} onClose={closeModal}>
+      <ModalTitle handleClose={closeModal}>Editing a product</ModalTitle>
       <ModalInputContainer errors={errors}>
-        {fieldsNames.map((fieldName, index) => (
-          <ModalInput
-            key={fieldName}
-            id={fieldName}
-            name={fieldName}
-            label={capitalizeFirstLetter(fieldName)}
-            variant="outlined"
-            error={Boolean(errors[fieldName])}
-            helperText={errors[fieldName]}
-            onChange={handleChange}
-            value={form[fieldName]}
-            autoFocus={index === 0}
-          />
-        ))}
+        <Input
+          name="store"
+          label="Store"
+          error={Boolean(errors.store)}
+          helperText={errors.store}
+          onChange={handleChange}
+          value={form.store}
+          autoFocus
+        />
+        <Input
+          name="price"
+          label="Price"
+          error={Boolean(errors.price)}
+          helperText={errors.price}
+          onChange={handleChange}
+          value={form.price}
+        />
+        <Input
+          name="productName"
+          label="Product name"
+          error={Boolean(errors.productName)}
+          helperText={errors.productName}
+          onChange={handleChange}
+          value={form.productName}
+        />
+        <Input
+          name="category"
+          label="Category"
+          error={Boolean(errors.category)}
+          helperText={errors.category}
+          onChange={handleChange}
+          value={form.category}
+        />
+        <Input
+          name="remains"
+          label="Remains"
+          error={Boolean(errors.remains)}
+          helperText={errors.remains}
+          onChange={handleChange}
+          value={form.remains}
+        />
+        <Input
+          name="weight"
+          label="Weight"
+          error={Boolean(errors.weight)}
+          helperText={errors.weight}
+          onChange={handleChange}
+          value={form.weight}
+        />
       </ModalInputContainer>
       <DialogActions>
         <StandardButton fullWidth onClick={handleSubmit}>
--- src/components/Modals/SellProduct.jsx
+++ src/components/Modals/SellProduct.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
+import DialogActions from "@mui/material/DialogActions";
+
+import useSales from "../../hooks/useSales";
+import useProducts from "../../hooks/useProducts";
+import useForm from "../../hooks/useForm";
 import ModalContainer from "./ModalContainer";
 import ModalTitle from "./ModalTitle";
 import ModalInputContainer from "./ModalInputContainer";
-import ModalInput from "./ModalInput";
-import { getFormatDate, haveErrors } from "../../utils/utils";
-import DialogActions from "@mui/material/DialogActions";
+import Input from "../Inputs/Input";
+import DateInput from "../Inputs/DateInput";
 import StandardButton from "../Buttons/StandardButton";
-import { useForm } from "../../hooks/useForm";
+import { haveErrors } from "../../utils/utils";
 import { Errors } from "../../consts/consts";
-import useSales from "../../hooks/useSales";
-import useProducts from "../../hooks/useProducts";
-import DateInput from "./DateInput";
 
 const initialStateErrors = { numberProducts: null, dateSale: null };
 
@@ -19,18 +20,18 @@ const initialStateForm = {
   dateSale: "",
 };
 
-const SellProduct = ({ open, closeModal, id }) => {
+const SellProduct = ({ open, closeModal, productId }) => {
   const [errors, setErrors] = useState({ ...initialStateErrors });
   const [form, setForm] = useForm({ ...initialStateForm });
-  const { addSale } = useSales();
-  const { products, updateProduct } = useProducts();
+  const { sales, addSale, updateSale } = useSales();
+  const { products, updateProduct, getProduct } = useProducts();
 
   const handleSubmit = () => {
     const { numberProducts, dateSale } = form;
 
     const numberProductsAsNumber = Number(numberProducts);
     const resultInsertedDate = String(dateSale["$d"]);
-    const oldProduct = products[id];
+    const oldProduct = products[productId];
     const restProducts = oldProduct.remains - numberProducts;
 
     const checkedErrors = {
@@ -51,18 +52,37 @@ const SellProduct = ({ open, closeModal, id }) => {
     const isNotErrors = haveErrors(checkedErrors);
 
     if (isNotErrors) {
-      const date = getFormatDate(resultInsertedDate);
+      const date = new Date(resultInsertedDate).getTime();
+      const product = getProduct(productId);
 
-      addSale({
-        ...products[id],
-        idProduct: id,
-        numberProducts: numberProductsAsNumber,
-        dateSale: date,
-      });
+      const existedSale = sales
+        ? Object.values(sales).find((sale) => sale.productId === productId)
+        : null;
 
-      updateProduct(id, {
-        ...products[id],
-        remains: products[id].remains - numberProductsAsNumber,
+      if (existedSale) {
+        updateSale(existedSale.id, {
+          soldItems: existedSale.soldItems + numberProductsAsNumber,
+          dateSale: date,
+        });
+      } else {
+        addSale({
+          productId,
+          accountId: product.accountId,
+          productName: product.productName,
+          store: product.store,
+          address: product.address,
+          category: product.category,
+          creationDate: product.creationDate,
+          price: product.price,
+          soldItems: numberProductsAsNumber,
+          weight: product.weight,
+          dateSale: date,
+        });
+      }
+
+      updateProduct(productId, {
+        ...products[productId],
+        remains: products[productId].remains - numberProductsAsNumber,
       });
 
       closeModal();
@@ -71,18 +91,23 @@ const SellProduct = ({ open, closeModal, id }) => {
     }
   };
 
-  const handleChange = ({ target }) => setForm({ [target.name]: target.value });
+  const handleChange = ({ target }) => {
+    setErrors((prevState) => ({ ...prevState, [target.name]: null }));
+    setForm({ [target.name]: target.value });
+  };
 
-  const handleChangeDate = (date) => setForm({ dateSale: date });
+  const handleChangeDate = (date) => {
+    setErrors((prevState) => ({ ...prevState, dateSale: null }));
+    setForm({ dateSale: date });
+  };
 
   return (
     <ModalContainer open={open} onClose={closeModal}>
       <ModalTitle handleClose={closeModal}>Sell the product</ModalTitle>
       <ModalInputContainer errors={errors}>
-        <ModalInput
+        <Input
           name="numberProducts"
           label="Number of products"
-          variant="outlined"
           error={Boolean(errors.numberProducts)}
           helperText={errors.numberProducts}
           onChange={handleChange}
