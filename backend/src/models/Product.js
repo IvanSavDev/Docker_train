2cadba3469993334ebe3de5f1b10907c027f4218
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-13 18:35:04 +0300

    feat: create user, sale and product models

---
+++ backend/src/models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    store: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    remains: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('Product', ProductSchema);
---
+++ backend/src/models/Sale.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const SaleSchema = new mongoose.Schema(
+  {
+    store: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    soldItems: {
+      type: Number,
+      required: true,
+    },
+    lastSale: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('Sale', SaleSchema);
