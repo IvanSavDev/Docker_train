ef3eca88d01faf5f32fd552a4b8005d9e03afd52
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-17 17:03:31 +0300

    feat: update Errors and add reg for password

--- backend/src/consts/consts.js
+++ backend/src/consts/consts.js
@@ -8,20 +8,53 @@ export const Errors = {
   Invalid_Data: 'Invalid data',
   Failed_Registration: 'Failed to registration',
   Failed_Authorization: 'Failed to authorization',
-  Authentication_Error: 'Authentication error',
-  Create_product: 'Error occurred during product creation',
-  Edit_product: 'Error while editing product',
-  Delete_product: 'Error while delete product',
-  Get_products: 'Error receiving products',
-  Not_found_product: 'Not found product',
-  Not_valid_product: 'Not valid product',
-  Get_sales: 'Error occurred while receiving sales',
-  Create_sale: 'Error occurred during sale creation',
-  Edit_sale: 'Error while editing sale',
-  Not_found_sales: 'Not found sales',
+  Failed_Authentication: 'Failed to authentication',
+  Failed_Identification: 'Wrong email or password',
+  More_zero: 'Value must be greater than 0',
+  More_one_symbol: 'Minimum 1 character required',
+  More_three_symbol: 'Minimum 3 character required',
+  Integer: 'Value must be integer and greater than 0',
+  Invalid_Date: 'Invalid Date',
+  Not_valid_address: 'Not valid address',
+};
+
+export const EmailErrors = {
+  EMAIL_EXIST: 'Email exist',
+  INVALID_EMAIL: 'Not valid email',
+};
+
+export const UserErrors = {
+  UPDATE_USER: 'Error during editing user',
+  NOT_VALID_USER: 'Not valid user data',
+};
+
+export const ProductErrors = {
+  CREATE_PRODUCT: 'Error occurred during product creation',
+  UPDATE_PRODUCT: 'Error during editing product',
+  DELETE_PRODUCT: 'Error during delete product',
+  GET_PRODUCTS: 'Error when receiving products',
+  NOT_FOUND_PRODUCT: 'Not found product',
+  NOT_FOUND_PRODUCTS: 'Not found products',
+  NOT_VALID_PRODUCT: 'Not valid product',
+};
+
+export const SaleErrors = {
+  GET_SALES: 'Error occurred during receiving sales',
+  CREATE_SALE: 'Error occurred during sale creation',
+  UPDATE_SALE: 'Error during editing sale',
+  NOT_FOUND_SALES: 'Not found sales',
+  NOT_FOUND_SALE: 'Not found sale',
   Not_valid_sale: 'Not valid sale',
-  Edit_user: 'Error while editing user',
-  Not_valid_user: 'Not valid user data',
+};
+
+export const PasswordErrors = {
+  SAME_PASSWORD: 'Same password',
+  EQUAL_OLD_PASSWORD: 'Does not equal old password',
+  INVALID_PASSWORD:
+    'Minimum 8 characters required, 1 special character and 1 number',
+  CONFIRMED_PASSWORD: 'Does not match the entered password',
+  EMPTY_NEW_PASSWORD: 'Required to enter a new password',
+  EMPTY_OLD_PASSWORD: 'Required to enter a old password',
 };
 
 export const Routes = {
@@ -33,3 +66,6 @@ export const Routes = {
   Registration: '/registration',
   User: '/user',
 };
+
+export const REG_PASSWORD =
+  /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
