eb2bacf305bc2bb9f7f4aa9fb51fc37d598a8418
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-17 17:02:51 +0300

    feat: update getFormatDate function

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -1,13 +1,2 @@
-export const getFormatDate = (dateForFormat) => {
-  const dateInMilliseconds = new Date(dateForFormat);
-  let day = dateInMilliseconds.getDate();
-  if (String(day).length === 1) {
-    day = `0${day}`;
-  }
-  let month = dateInMilliseconds.getMonth() + 1;
-  if (String(month).length === 1) {
-    month = `0${month}`;
-  }
-  const year = dateInMilliseconds.getFullYear();
-  return `${day}.${month}.${year}`;
-};
+export const getFormatDate = (dateForFormat) =>
+  new Date(dateForFormat).toLocaleDateString();
