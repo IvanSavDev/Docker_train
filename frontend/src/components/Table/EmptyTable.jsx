5b4b95aaec328893fd8c7ef9a83c2a4c0d21bcbb
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-20 18:37:43 +0300

    feat: delete account, products, sales providers and emptyTable

--- frontend/src/components/Table/EmptyTable.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './EmptyTable.module.css';
-
-const EmptyTable = ({ children }) => (
-  <div className={styles.container}>{children}</div>
-);
-
-export default EmptyTable;
