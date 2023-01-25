0bff706684cbe0aa0b97b52471019087c7e37d94
Ivan Savenko <iv.savenko@it-justice.com>
2023-01-25 18:49:53 +0300

    feat: add left sidebar for burger menu

---
+++ frontend/src/components/Sidebar/LeftSidebar.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { styled } from '@mui/material/styles';
+import { Drawer } from '@mui/material';
+
+import { NavList } from './NavList';
+import { Logo } from './Logo';
+import { throttle } from '../../utils/utils';
+
+const StyledDrawer = styled(Drawer)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '40px 0 0 0',
+    gap: '20px',
+  },
+  [theme.breakpoints.up('middle')]: {
+    display: 'none',
+  },
+}));
+
+export const LeftSidebar = ({ open, handleClose }) => {
+  useEffect(() => {
+    const checkWindowResize = (event) => {
+      if (event.target.innerWidth > 720) {
+        handleClose();
+      }
+    };
+
+    const handleResize = throttle(checkWindowResize, 500);
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <StyledDrawer open={open} onClose={handleClose}>
+      <Logo />
+      <NavList handleClose={handleClose} />
+    </StyledDrawer>
+  );
+};
